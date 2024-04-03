08522a937468335bfeedaeda3b554f639b17c6da
Marcio Silva <marcio.silva.ter@estacio.br>
2024-04-03 16:21:15 -0300

    API Autor

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { AutorModule } from './autor/autor.module';
+
+@Module({
+  imports: [
+    AutorModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
+})
+export class AppModule {}
